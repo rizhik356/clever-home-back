8172163e1602d2c671df81fd48810bb1463271d8
Dmitry <dmitry@MacBook-Pro-Dmitry.local>
2026-03-17 20:36:10 +0300

    fix err

--- src/devices-logs/devices-logs.module.ts
+++ src/devices-logs/devices-logs.module.ts
@@ -4,11 +4,12 @@ import { SequelizeModule } from '@nestjs/sequelize';
 import { DevicesLogs } from './devices-logs.model';
 import { UserDevices } from '../devices/user-devices.model';
 import { DevicesLogsController } from './devices-logs.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [DevicesLogsController],
   providers: [DevicesLogsService],
-  imports: [SequelizeModule.forFeature([DevicesLogs, UserDevices])],
+  imports: [SequelizeModule.forFeature([DevicesLogs, UserDevices]), AuthModule],
   exports: [DevicesLogsService],
 })
 export class DevicesLogsModule {}
